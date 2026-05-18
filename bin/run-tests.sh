ec034bb1d6bdbcefa67ff45bac1c1f703f5fc041
Eric Willigers <keiravillekode@gmail.com>
2026-05-18 11:54:51 +1000

    Compare against expected_results.json

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+exit_code=0
+
 for test in test/*; do
     for testdir in $test/*; do
         testname="$(basename $testdir)"
@@ -7,6 +9,18 @@ for test in test/*; do
         output_dir=$testdir
         if [ -d $testdir ]; then
             bin/run.sh $testname $testdir $output_dir
+
+            # if ! diff "${output_dir}/results.json" "${output_dir}/expected_results.json"; then
+
+            diff <(jq --sort-keys . "${output_dir}/results.json") \
+                 <(jq --sort-keys . "${output_dir}/expected_results.json")
+
+            if [ $? -ne 0 ]; then
+                echo "❌ ${testdir} FAILED"
+                exit_code=1
+            fi
         fi
     done
 done
+
+exit ${exit_code}
