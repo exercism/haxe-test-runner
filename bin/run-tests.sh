60a97fcffa6618ed4659dacf071979ae6ec5a004
Eric Willigers <keiravillekode@gmail.com>
2026-05-18 14:12:59 +1000

    feedback

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -3,19 +3,19 @@
 exit_code=0
 
 for test in test/*; do
-    for testdir in $test/*; do
-        testname="$(basename $testdir)"
+    for testdir in "$test"/*; do
+        testname="$(basename "$testdir")"
         testname=${testname//_/-}
         output_dir=$testdir
-        if [ -d $testdir ]; then
-            bin/run.sh $testname $testdir $output_dir
+        if [ -d "$testdir" ]; then
+            bin/run.sh "$testname" "$testdir" "$output_dir"
 
             # if ! diff "${output_dir}/results.json" "${output_dir}/expected_results.json"; then
 
             diff <(jq --sort-keys . "${output_dir}/results.json") \
                  <(jq --sort-keys . "${output_dir}/expected_results.json")
 
-            if [ $? -ne 0 ]; then
+            if (( $? != 0 )); then
                 echo "❌ ${testdir} FAILED"
                 exit_code=1
             fi
